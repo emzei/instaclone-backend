b0c6e49fa63727dda13dbd26c2d4b63de12d105e
emzei <emzei.j@gmail.com>
2022-09-12 21:53:29 +0900

    Update server.js by using graphql

--- server.js
+++ server.js
@@ -3,17 +3,37 @@ import { ApolloServer, gql } from "apollo-server";
 
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title:String!): Boolean
+    deleteMovie(title:String!): Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'hiiii',
+    movies: () => [],
+    movie: () => ({ "title": "Hello", year: 2022 }),
   },
+  Mutation: {
+    // root, args, context, info
+    createMovie: (_, {title}) => {
+      console.log(title);
+      return true
+    },
+    deleteMovie: (_, {title}) => {
+      console.log(title);
+      return true      
+    }
+  }
 };
 
 const server = new ApolloServer({
