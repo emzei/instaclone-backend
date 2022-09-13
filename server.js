8571e29f3aeb94aa88bc903877746a6be35bc804
emzei <emzei.j@gmail.com>
2022-09-13 21:52:30 +0900

    Update Prisma client
    - gql: exclamation mark means REQUIRED
    - schema: question mark means NOT REQUIRED

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-//const { ApolloServer, gql } = require('apollo-server');
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient()
 
 // The GraphQL schema
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
     movie: Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
     deleteMovie(title: String!): Boolean
   }
 `;
@@ -21,15 +26,19 @@ const typeDefs = gql`
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    movies: () => [],
+    movies: () => client.movie.findMany(),
     movie: () => ({ title: "Hello", year: 2022 }),
   },
   Mutation: {
     // root, args, context, info
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
     deleteMovie: (_, { title }) => {
       console.log(title);
       return true;
