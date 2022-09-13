e796bcd6b758d6e55901f8327aaee5c315599dad
emzei <emzei.j@gmail.com>
2022-09-13 23:34:20 +0900

    Setup dotenv

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
 const server = new ApolloServer({
-  schema
+  schema,
 });
 
-server.listen().then(() => {
-  console.log(`🚀 Server ready at http://localhost:4000/`);
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}/`);
 });
