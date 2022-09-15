4810a368de023ed3e0aa00f297ec8bfdcc57b12a
emzei <emzei.j@gmail.com>
2022-09-16 01:02:44 +0900

    Divide and conquer

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import client from "../client";
+import client from "../../client";
 
 export default {
   Mutation: {
@@ -44,41 +44,6 @@ export default {
         return e;
       }
     },
-
-    login: async (_, { username, password }) => {
-      try {
-        // find user with args.username
-        const user = await client.user.findFirst({
-          where: {
-            username, // <=> username:username
-          },
-        });
-
-        if (!user) {
-          return {
-            ok: false,
-            error: "User not found",
-          }
-        }
-
-        // check password with args.password
-        const passwordOk = await bcrypt.compare(password, user.password);
-        
-        if (!passwordOk) {
-          return {
-            ok: false,
-            error: "Incorrect Password",
-          };
-        }
-        
-        // issue a token and send it to the user
-        const token = await jwt.sign({id:user.id}, process.env.SECRET_KEY);
-        return {
-          ok: true,
-          token,
-          error: false
-        }
-      } catch (e) {}
-    },
   },
 };
+
